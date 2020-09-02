9290b0ed69351b451ca1c12dab0c217b4a85f3f6
Laurent Tupin <64402274+Laurent-Tupin@users.noreply.github.com>
2020-09-02 11:40:14 +0800

    Update 1_MachineLearning.py

--- 5_DataCamp/Data Science/1_MachineLearning.py
+++ 5_DataCamp/Data Science/1_MachineLearning.py
@@ -26,27 +26,39 @@ import seaborn as sb
 
 plt.style.use('ggplot')
 
+def fXy_transformDfIntoNpArr(df, str_targetName):
+    y = df[str_targetName].values
+    X = df.drop(str_targetName, axis=1).values
+    return X, y
+
+def dDf_TransformXinDf(X, l_feature_names):
+    df = pd.DataFrame(X, columns = l_feature_names)
+    return df
+
 def IrisData():
     from sklearn import datasets
     iris = datasets.load_iris()
     #    print(iris.keys())
-    print(iris.target_names)
+    l_feature_names = iris.feature_names
+    l_target_names = iris.target_names
     # Build the data model
     X = iris.data
     y = iris.target
-    #    print(type(X), type(y))
-    df = pd.DataFrame(X, columns = iris.feature_names)
-    return df, y
+    return X, y, l_feature_names, l_target_names
 
 def cours1_scatter_matrix():
-    df, y = IrisData()
+    X, y, l_feature_names, l_target_names = IrisData()
+    print(l_target_names)
+    df = dDf_TransformXinDf(X, l_feature_names)
     # Some Chart / plot
     pd.plotting.scatter_matrix(df, c = y, s = 100, marker = 'D', figsize = [12,8])
-#cours1()
+#cours1_scatter_matrix()
 
 def EDA_countPlot():
-    df, y = IrisData()
-    df['flower'] = y
+    X, y, l_feature_names, l_target_names = IrisData()
+    print(l_target_names)
+    df = dDf_TransformXinDf(X, l_feature_names)
+    df['flower'] = y    
     # Some Chart / plot
     for xx in ['sepal length (cm)', 'sepal width (cm)', 'petal length (cm)','petal width (cm)']:
         #   Be sure to begin your plotting statements for each figure with plt.figure() 
@@ -59,14 +71,18 @@ def EDA_countPlot():
 #EDA_countPlot()
 
 def ClassificationChallenge():
-    df, y = IrisData()
+    X, y, l_feature_names, l_target_names = IrisData()
+    print(l_target_names)
     # K-Nearest neighbors
     from sklearn.neighbors import KNeighborsClassifier
     knn = KNeighborsClassifier(n_neighbors = 6)
-    knn.fit(df, y)
+    knn.fit(X, y)
     y_predict = knn.predict(np.array([[5.6,2.8,3.9,1.1], 
                                       [5.7, 2.6, 3.8, 1.3], 
                                       [4.7, 3.2, 1.3, 0.2]]))
+    #    KNeighborsClassifier(algorithm='auto', leaf_size=30, metric='minkowski',
+    #                         metric_params=None, n_jobs=1, n_neighbors=6, p=2,
+    #                         weights='uniform')
     return y_predict
 
 y_predict = ClassificationChallenge()
