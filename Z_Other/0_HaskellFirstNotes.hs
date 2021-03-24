cbd614ea3a5d1a67da192ef2e15ffd2062decb76
Laurent Tupin <laurent.tupin@ihsmarkit.com>
2021-03-24 19:30:18 +0800

    Create 0_HaskellFirstNotes.hs

---
+++ Z_Other/0_HaskellFirstNotes.hs
@@ -0,0 +1,40 @@
+--https://www.youtube.com/watch?v=02_H3LjqMr8&list=PLYIxT-vN4kRpGt0F4j5PL5ggFtYYnoxdn&index=7&t=27s
+
+-- Init Terminal to compile everything
+gsci
+:l haskell-tut
+:r
+
+
+-- Import
+import Data.List
+import System.IO
+
+
+--DATA Type
+
+--INt
+maxInt = maxBound :: Int	--  -2^63
+minInt = minBound :: Int	--  2^63
+
+--Integer: No Bound
+--Float
+--Double
+bigFloat = 3.99999999999 + 0.0000000005
+
+--Bool
+--Char
+--Tuple
+
+
+--CONST variable
+always5 :: Int
+always5 = 5
+
+--SUM
+sumOfNums = sum[1..1000]
+
+
+
+
+
