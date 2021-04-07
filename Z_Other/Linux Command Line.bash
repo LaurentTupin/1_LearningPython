a5a757b8211355adec2d0bf6e90b002cb5bac0e5
Laurent Tupin <laurent.tupin@ihsmarkit.com>
2021-04-07 19:46:11 +0800

    Create Linux Command Line.bash

---
+++ Z_Other/Linux Command Line.bash
@@ -0,0 +1,99 @@
+# GIT
+sudo apt install git
+git clone git://github.com/scottsimpson/commandlinebasics
+
+# Structure
+Command: ls
+Options: -lh
+Arguments: /usr/bin
+
+# Text Navigation Shortcuts
+press tab TWICE to have suggestions of command that start with the char you filled
+CTL + A: move to begining of line
+CTL + E: move to end of line
+CTL + <= : move to left one word
+CTL + => : move to right one word
+CTL + U: Crop from cursor to start
+CTL + K: Crop from cursor to end
+CTL + Y: Paste Cropped Text
+CTL + SHIFT + C: Copy to clipboard
+CTL + SHIFT + V: Paste from clipboard
+Up Arrow: Recall previous commands
+Down Arrow: Scroll previous commands
+CTL + R: Search commands history
+CTL + C: Cancel command
+
+# Documentation
+h
+	# help
+man ls
+ls --help
+	# will tell information about a command
+apropos
+	# give list of command and their description
+	
+
+# Commands
+ls
+	# list content of directory
+ls -l
+	# long listing
+	#  show file with its characteristics
+ls -l Desktop/
+	# long listing in Desktop/
+clear
+	# clear the screen
+
+# FILE
+file txtFile.txt
+	# kind of file it is
+stat txtFile.txt
+	# extend information of the file
+cd
+	# change directory
+cd.. 
+	# go a folder upward
+cd../..
+	# go 2 folder upward
+cd../../finance
+	# go 2 folder upward + Go to folder Finance
+pwd
+	# check where you are : your current directory
+cd -
+	# going back to the previous current directory you were in
+cd
+	# go back to your home folder
+
+# Focus on ls
+ls
+	# list content of directory
+ls -l
+	# long listing
+	#  	show file with its characteristics
+	#	d/-   directory / file
+	#	Authorization
+	# 	size in BITES
+ls -lh
+	# size readable: k , M, G
+	
+
+
+
+
+
+
+
+
+
+
+
+2. For a no-show (that NEVER happens anyway) a high majority is in favor of sanction: 75%
+The highest result (40%) is in favor of exclusion for the next game.
+Let's replace that by an whole hour in goal !!!
+An exclusion is no Sanction to our friends outside of IHS because they already are coming partially.
+Once again... It never happens so it should not be an issue.
+
+
+
+
+
